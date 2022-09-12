8dcf5a00c4173cff9b731f4f7400a338e5d32265
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2022-09-12 08:34:02 +0000

    Starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const date = new Date();
+  const hours = date.getHours()
+  let timeOfDay
+
+  if (hours > 12){
+    timeOfDay = "Good morning"
+  }else if (hours >= 12 && hours < 17){
+timeOfDay = "Good afternoon"
+  }else{
+    timeOfDay = "Good evening"
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{timeOfDay}</h1>
     </div>
   );
 }
